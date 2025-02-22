3c8f5e21ab2a21f228a7aadf19a8ab9198c8f46e
roshan.birwatkar <roshan.birwatkar@gmail.com>
2025-02-23 00:42:25 +0530

    Added doggo-rating-app and other libraries from "end" of m04

--- doggo-rating-workspace-re-arch-with-libs/apps/doggo-rating-app/src/app/app.component.ts
+++ doggo-rating-workspace-re-arch-with-libs/apps/doggo-rating-app/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AuthActions } from '@ps-doggo-rating/shared/util-auth';
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
-  selector: 'doggo-rating-workspace-re-arch-with-libs-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrls: ['./app.component.css'],
+  imports: [LayoutComponent],
 })
-export class AppComponent {
-  title = 'doggo-rating-app';
+export class AppComponent implements OnInit {
+  title = 'ratemydoggo';
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.checkAuth();
+  }
+
+  private checkAuth() {
+    this.store.dispatch(AuthActions.checkAuth());
+  }
 }
